618148d6d8f8c7e17a5209b4e2260732e7356cf3
Rko_8001 <yadwindersingh0705@gmail.com>
2023-11-23 19:35:01 +0530

    minor edit

--- frontend/src/components/NegativeResult.js
+++ frontend/src/components/NegativeResult.js
@@ -52,7 +52,7 @@ export default function NegativeResult() {
             </div>
             <div className="mt-10 text-center items-center">
                 <button 
-                    className="w-32 flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" 
+                    className="w-40 flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" 
                     onClick={getRecoveryData}
                 >
                     Recover Data
